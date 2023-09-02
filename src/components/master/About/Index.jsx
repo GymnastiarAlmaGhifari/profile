f9a3cd7105e85fa7ef6ef2b3821ad99b0c12cd23
E41211478 <e41211478@student.polije.ac.id>
2023-09-02 20:05:04 +0700

    robothand

--- src/components/master/About/Index.jsx
+++ src/components/master/About/Index.jsx
@@ -1,11 +1,11 @@
 import Overview from "./Overview";
 import { SectionWrapper } from "../../../hoc";
-// import RobotHand from "./RobotHand";
+import RobotHand from "./RobotHand";
 
 const About = () => {
   return (
     <div className="flex flex-col md:flex-row items-center gap-5 justify-between   md:snap-center">
-      {/* <RobotHand /> */}
+      <RobotHand />
 
       <Overview />
     </div>
